0558152825dc8a66f4bcb38ec3ffb0ff0c15ff9a
Brendan Wilde <brendanwilde@gmail.com>
2015-11-13 13:59:14 +0000

    completed part 2, adding in filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,47 +1,55 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
+	def movie_params
+		params.require(:movie).permit(:title, :rating, :description, :release_date)
+	end
 
-  def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
-  end
+	def show
+		id = params[:id] # retrieve movie ID from URI route
+		@movie = Movie.find(id) # look up movie by unique ID
+		# will render app/views/movies/show.<extension> by default
+	end
 
-  def index
-    @sort = params[:sort]
-    @highlight_class = "hilite"
-    @movies = Movie.all.order(@sort)
-  end
+	def index
+		@sort = params[:sort]
+		if params[:ratings] then @ratings = params[:ratings].keys end
+		@highlight_class = "hilite"
 
-  def new
-    # default: render 'new' template
-  end
+		if @ratings.nil?
+			@movies = Movie.all.order(@sort)
+		else
+			@movies = Movie.where(rating: @ratings).order(@sort)
+		end
 
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
+		@all_ratings = Movie.uniq.pluck(:rating).sort
+	end
 
-  def edit
-    @movie = Movie.find params[:id]
-  end
+	def new
+		# default: render 'new' template
+	end
 
-  def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
-  end
+	def create
+		@movie = Movie.create!(movie_params)
+		flash[:notice] = "#{@movie.title} was successfully created."
+		redirect_to movies_path
+	end
 
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
-    flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path
-  end
+	def edit
+		@movie = Movie.find params[:id]
+	end
+
+	def update
+		@movie = Movie.find params[:id]
+		@movie.update_attributes!(movie_params)
+		flash[:notice] = "#{@movie.title} was successfully updated."
+		redirect_to movie_path(@movie)
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		flash[:notice] = "Movie '#{@movie.title}' deleted."
+		redirect_to movies_path
+	end
 
 end
